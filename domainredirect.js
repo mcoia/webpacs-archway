2423d208b5a9633a221ecf03519e329123ebc5d7
hammondjn <jessica@mobiusconsortium.org>
2012-01-19 15:50:18 -0600

    Styled the redirect message

--- domainredirect.js
+++ domainredirect.js
@@ -20,7 +20,7 @@ if(href.indexOf("missouri.edu") != -1 ){
 }else{}
 
 if (href.indexOf("&redirected") != -1 ) {
-    var redirectMessage = "<p>You've been redirected from archway.mobius.umsystem.edu or archway.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to http://archway.searchmobius.org.</p>"
+    var redirectMessage = "<p>You've been redirected from archway.mobius.umsystem.edu or archway.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://archway.searchmobius.org'>http://archway.searchmobius.org</a>.</p>"
 
     $(document).ready(function () {
         $('#redirectMessage').html(redirectMessage);
