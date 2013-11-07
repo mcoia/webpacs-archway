1172b0d1b904fd73328c2b09bde751d0eee473fd
Megan Durham <megan@mobiusconsortium.org>
2013-11-07 16:45:01 -0600

    add 856links js

---
+++ 856links_s10.js
@@ -0,0 +1,35 @@
+function hideLinks(){
+    
+    var stringsToHide = new Array ();
+    stringsToHide[1] = new Array ("eastcentral");
+    stringsToHide[2] = new Array ("jeffco");
+    stringsToHide[3] = new Array ("stchas");
+    stringsToHide[10] = new Array ("stlcc");
+    stringsToHide[9] = new Array ("stlcop");
+
+
+
+    var scopeDropdown = $("#searchscope").val();
+    
+    delete stringsToHide[scopeDropdown];
+    
+    if ($("table").is('.bibLinks')){
+    for (var k in stringsToHide) {
+        for (var l in stringsToHide[k]) {
+        $(".bibLinks a").each(function(index, element) {
+            if($(element).parent().html().indexOf(stringsToHide[k][l]) > -1) {
+            $(element).parent().parent().hide()
+            }    
+        });
+        }
+    }
+    if ($(".bibLinks tr[style*=none]").length < $(".bibLinks a").length){
+        $(".bibLinks").show();       
+    }
+    }  
+}
+    
+    
+    $(document).ready(function () {
+    hideLinks();
+});
