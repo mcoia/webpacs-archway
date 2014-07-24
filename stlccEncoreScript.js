f01e6f7a1077fe27a0a3a1e02ce7ef38b5cab735
Megan Durham <megan@mobiusconsortium.org>
2014-07-24 16:28:59 -0500

    hide all spans

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -1,11 +1,8 @@
 window.onload = init;
-//hides everything as long as there are only 3 856s on the page
+//hides everything 
 function init(){
-
     var divsToHide = document.getElementsByClassName("addtlInfoItem");
-
-    for(var i = 0; i < divsToHide.length; i++)
-    {
+    for(var i = 0; i < divsToHide.length; i++){
     divsToHide[i].style.visibility="hidden";
     } 
 }
