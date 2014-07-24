a9846b3c200d0fe61a22eaa2ebb97c0d45743184
Megan Durham <megan@mobiusconsortium.org>
2014-07-24 16:22:24 -0500

    hides all strings but less code

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -1,16 +1,8 @@
 window.onload = init;
 //hides everything as long as there are only 3 856s on the page
 function init(){
-var a = document.getElementById("eight56LinkComponent");
-a.classList.add("otherclass");
-var b = document.getElementById("eight56LinkComponent_0");
-b.classList.add("otherclass");
-var c = document.getElementById("eight56LinkComponent_1");
-c.classList.add("otherclass");
-hideStlcc();
-}
-function hideStlcc(){
-    var divsToHide = document.getElementsByClassName("otherclass");
+
+    var divsToHide = document.getElementsByClassName("addtlInfoItem");
 
     for(var i = 0; i < divsToHide.length; i++)
     {
