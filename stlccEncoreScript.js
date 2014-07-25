495f125c35da3a9241cf0a46d31e70942f860792
Megan Durham <megan@mobiusconsortium.org>
2014-07-25 17:30:52 -0500

    need to undo

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -3,8 +3,5 @@ jQuery(document).ready(function(){
 	jQuery(".hideMe").hide();
 	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').length) {
 		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
-    
-    //this appends the first div too all the classes on the page -- need to match item container number id to all items id number
-	jQuery("div[id*='allitems-']").clone().appendTo(".availabilitySummaryArea").addClass('contentEn bibHoldingsWrapperEn popUpWrapperEn');
 });
 
