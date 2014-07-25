bc6d7a65f8d179e6b6bd124c7dcde1092b06c5c9
Megan Durham <megan@mobiusconsortium.org>
2014-07-25 13:34:26 -0500

    find & replace txt for na msg for online items

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function(){
 	jQuery('span .addtlInfoItem:not(:contains("STLCC"))').addClass('hideMe');
 	jQuery(".hideMe").hide();
+	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').length) {
+		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
 });
 
