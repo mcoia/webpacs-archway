c689b03928bb6b13e10dfd1982c942535b9fdd68
Megan Durham <megan@mobiusconsortium.org>
2014-07-25 13:40:22 -0500

    add code for scc page

--- sccEncoreScript.js
+++ sccEncoreScript.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function(){
 	jQuery('span .addtlInfoItem:not(:contains("St. Charles"))').addClass('hideMe');
 	jQuery(".hideMe").hide();
+	if (jQuery('span .availabilityMessage:contains("St Charles Elec. Resource")').length) {
+		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
 });
