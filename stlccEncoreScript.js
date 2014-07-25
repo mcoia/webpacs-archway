234fca083616cce280602035364a1c95a54810dc
Megan Durham <megan@mobiusconsortium.org>
2014-07-25 16:27:28 -0500

    getting closer to jquery also match styling

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -3,7 +3,8 @@ jQuery(document).ready(function(){
 	jQuery(".hideMe").hide();
 	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').length) {
 		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
-//this appends ALL of these(.availableItemsSection) on page to EVERY div(.availabilitySummaryArea)
-	//jQuery(".availableItemsSection").clone().appendTo(".availabilitySummaryArea");
+    
+    //this appends the first div too all the classes on the page -- need to match item container number id to all items id number
+	jQuery("div[id*='allitems-']").clone().appendTo(".availabilitySummaryArea").addClass('contentEn bibHoldingsWrapperEn popUpWrapperEn');
 });
 
