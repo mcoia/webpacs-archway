e91a88e282233160098cf196fbe2f4ce808a1010
Megan Durham <megan@mobiusconsortium.org>
2014-07-25 15:06:25 -0500

    trying to add jquery for see all button

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -3,5 +3,7 @@ jQuery(document).ready(function(){
 	jQuery(".hideMe").hide();
 	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').length) {
 		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
+//this appends ALL of these(.availableItemsSection) on page to EVERY div(.availabilitySummaryArea)
+	//jQuery(".availableItemsSection").clone().appendTo(".availabilitySummaryArea");
 });
 
