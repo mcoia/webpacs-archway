de5aa97f879370b1a9f2815643a637753fb14fac
Megan Durham <megan@mobiusconsortium.org>
2014-10-23 14:15:22 -0500

    update js for electronic resources

--- stlccEncoreScript.js
+++ stlccEncoreScript.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
 	jQuery('span .addtlInfoItem:not(:contains("STLCC"))').addClass('hideMe');
 	jQuery(".hideMe").hide();
-	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').length) {
-		jQuery('span .itemsNotAvailable').text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
-});
+	if (jQuery('span .availabilityMessage:contains("STLCC Electronic Resources")').addClass('change').length) {
+    jQuery("span.change").find("span").text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
+  });
+
 
