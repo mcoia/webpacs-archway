b7af6f4bd92cfb9e9232119a730e2f6574ab7d90
Megan Durham <megan@mobiusconsortium.org>
2014-10-28 16:19:34 -0500

    add js to show gutenberg items

--- sccEncoreScript.js
+++ sccEncoreScript.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
 	jQuery('span .addtlInfoItem:not(:contains("St. Charles"))').addClass('hideMe');
 	jQuery(".hideMe").hide();
+	jQuery('span .addtlInfoItem:contains("Freely available online")').addClass('showMe');
+	jQuery(".showMe").show();
 	if (jQuery('span .availabilityMessage:contains("St Charles Elec. Resource")').addClass('change').length) {
     jQuery("span.change").find("span").text('Available').removeClass('itemsNotAvailable').addClass('itemsAvailable')};
   });
